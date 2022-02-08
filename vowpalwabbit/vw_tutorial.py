2c71bdf6f689b5c24bb7e8721c453bf4ca2073f3
Mario Aburto <maburto00@gmail.com>
2022-02-08 15:46:24 +0900

    vm_tutorial file added

--- vowpalwabbit/vw_tutorial.py
+++ vowpalwabbit/vw_tutorial.py
@@ -1,6 +1,6 @@
 from vowpalwabbit import pyvw
 
-model = pyvw.vw(quiet=False)
+model = pyvw.vw(quiet=True)
 train_examples = [
   "0 | price:.23 sqft:.25 age:.05 2006",
   "1 | price:.18 sqft:.15 age:.35 1976",
